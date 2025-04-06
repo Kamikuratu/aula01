23ae1fe6a9742a920616af3bc0e77e3a03304cb0
Raphael Ramos <raphael_grecco@hotmail.com>
2025-04-06 19:42:26 -0300

    Primeiros exercícios da aula 4

---
+++ aula 4/exercicios.py
@@ -0,0 +1,40 @@
+#Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+
+#lista = list(range(1, 11))
+
+#for numero in lista:
+#    print(numero ** 2)
+
+
+#Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+
+#lista = ['Python', 'Java', 'C++', 'JavaScript']
+
+#for i in range(len(lista)):
+#    if lista[i] == 'C++':
+#        lista[i] = 'Ruby'
+#print(lista)
+
+#Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+
+#dic = {'titulo': 'Harry Potter', 'autor': 'J. K. Rowling', 'ano': 1996}
+
+#for chave, valor in dic.items():
+#    print(f"{chave} {valor}")
+
+#Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
+
+#string: str = 'Raphael'
+#caracters = list(string)
+#dic = {i:string.count(i) for i in caracters}
+#print(dic)
+
+
+#Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
+
+lista = ["maçã", "banana", "cereja"]
+precos = {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}
+
+total = sum(precos[item] for item in lista )
+
+print(total)
